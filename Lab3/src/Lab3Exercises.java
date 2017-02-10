75edeb4e04134743a588a02dec8b065b5399716b
3010a11 <3010a11@mamey.ece.uprm.edu>
2017-02-10 12:27:08 -0400

    THE FLAG

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -5,7 +5,7 @@ public class Lab3Exercises {
 		JFrame myFrame = new JFrame("Xavo!");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(850, 400);
-		myFrame.setSize(200, 200);
+		myFrame.setSize(200, 160);
 	    MyPanelClass myPanel = new MyPanelClass();
         myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
